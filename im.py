0a52aa76534495f5918e31b830065bb92a960d29
amir78729 <amir78729@gmail.com>
2020-08-15 16:45:27 +0430

    bugs fixed

--- im.py
+++ im.py
@@ -1,8 +1,6 @@
 import numpy as np
 import cv2
 
-# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
-
 #https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
