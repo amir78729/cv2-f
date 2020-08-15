8235515caa70cb6724de3a9f84b12dd55ae32804
amir78729 <amir78729@gmail.com>
2020-08-15 16:27:47 +0430

    canny added

--- im.py
+++ im.py
@@ -18,8 +18,6 @@ cap = cv2.VideoCapture(0)
 while 1:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
@@ -29,20 +27,23 @@ while 1:
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-        # smile = smile_cascade.detectMultiScale(roi_gray)
-        # for (ex,ey,ew,eh) in smile:
-        #     cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,0),2)
-    uppperbody = upperbody_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
-
     cv2.imshow('img',img)
+
+    ret, frame = cap.read()
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    blurred_image = cv2.GaussianBlur(gray, (7,7), 0)
+    canny = cv2.Canny(blurred_image, 25, 150)
+    cv2.imshow("Canny with high thresholds", canny)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
+
 
 cap.release()
 cv2.destroyAllWindows()
