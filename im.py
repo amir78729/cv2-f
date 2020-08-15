77064ccf0d69ed53bb34884b39a1320375b41b09
amir78729 <amir78729@gmail.com>
2020-08-15 17:43:26 +0430

    converting regtangulars to citculs

--- im.py
+++ im.py
@@ -19,12 +19,14 @@ while 1:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.circle(img,(int(x+w/2),int(y+h/2)),100,(255,0,0),2)
+        # cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            # cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            cv2.circle(roi_color,(int(ex+ew/2),int(ey+eh/2)),50,(0,0,255),2)
     cv2.imshow('img',img)
 
     #black and white
