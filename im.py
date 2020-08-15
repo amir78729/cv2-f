ab5f55610f1f3212e1b1cd280df78fc7734fc42b
amir78729 <amir78729@gmail.com>
2020-08-15 16:30:22 +0430

    canny added

--- im.py
+++ im.py
@@ -16,6 +16,7 @@ upperbody_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
 cap = cv2.VideoCapture(0)
 
 while 1:
+    #face and eyes recognition
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -23,55 +24,23 @@ while 1:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-        
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
     cv2.imshow('img',img)
 
+    #black and white
     ret, frame = cap.read()
-    # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     blurred_image = cv2.GaussianBlur(gray, (7,7), 0)
     canny = cv2.Canny(blurred_image, 25, 150)
     cv2.imshow("Canny with high thresholds", canny)
-
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
 
-
 cap.release()
 cv2.destroyAllWindows()
-
-# cap.release
-
-
-
-# import numpy as np
-# import cv2
-
-# cap = cv2.VideoCapture(0)
-
-# while(True):
-#     # Capture frame-by-frame
-#     ret, frame = cap.read()
-
-#     # Our operations on the frame come here
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-#     blurred_image = cv2.GaussianBlur(gray, (7,7), 0)
-#     canny = cv2.Canny(blurred_image, 25, 150)
-#     cv2.imshow("Canny with high thresholds", canny)
-
-
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-
-# # When everything done, release the capture
-# cap.release()
-# cv2.destroyAllWindows()
